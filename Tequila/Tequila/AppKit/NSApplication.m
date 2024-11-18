1f6c6e8f77e5e6e0cbdca65dfdc635cba4edf5dd
Samuliak <samuliak77@gmail.com>
2024-11-18 15:25:45 +0100

    initial commit

---
+++ Tequila/Tequila/AppKit/NSApplication.m
@@ -0,0 +1,13 @@
+//
+//  NSApplication.m
+//  Tequila
+//
+//  Created by Samuliak on 18/11/2024.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+extern int NSApplicationMain(int argc, const char* _Nonnull argv[_Nonnull]) {
+    return UIApplicationMain(argc, argv, @"UIApplication", nil);
+}
