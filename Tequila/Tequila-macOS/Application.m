d84dfca3e93d1321b17c51f43253ad07f2116614
Samuliak <samuliak77@gmail.com>
2024-11-23 13:37:15 +0100

    support macOS builds

---
+++ Tequila/Tequila-macOS/Application.m
@@ -0,0 +1,10 @@
+//
+//  Application.m
+//  Tequila-macOS
+//
+//  Created by Samuliak on 23/11/2024.
+//
+
+#include "Tequila.h"
+
+int tqlApplicarionMain(int argc, const char* _Nonnull argv[_Nonnull]) { return 0; }
