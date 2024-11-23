8ec9ec8095ccc5127fa1141b5e3b64ee21ecf6c2
Samuliak <samuliak77@gmail.com>
2024-11-23 12:23:55 +0100

    don't implement AppKit symbols directly

--- Tequila/Tequila/Application.m
+++ Tequila/Tequila/Application.m
@@ -1,13 +1,17 @@
 //
-//  NSApplication.m
+//  Application.m
 //  Tequila
 //
 //  Created by Samuliak on 18/11/2024.
 //
 
+#include "Tequila.h"
+
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
-extern int NSApplicationMain(int argc, const char* _Nonnull argv[_Nonnull]) {
+int tqlApplicarionMain(int argc, const char* _Nonnull argv[_Nonnull]) {
+    [[NSClassFromString(@"AppDelegate") alloc] init];
+    
     return UIApplicationMain(argc, argv, @"UIApplication", nil);
 }
---
+++ Tequila/Tequila/Tequila.h
@@ -0,0 +1,8 @@
+//
+//  Tequila.h
+//  Tequila
+//
+//  Created by Samuliak on 23/11/2024.
+//
+
+extern int tqlApplicarionMain(int argc, const char* _Nonnull argv[_Nonnull]);
