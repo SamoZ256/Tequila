3ad169155f52b86ac0dfb9337d83b8363dc4da8a
Samuliak <samuliak77@gmail.com>
2024-11-24 09:30:26 +0100

    reorganize the files

--- Tequila/Application.m
+++ Tequila/Application.m
@@ -11,6 +11,8 @@
 #import <UIKit/UIKit.h>
 
 int tqlApplicarionMain(int argc, const char* _Nonnull argv[_Nonnull]) {
+    while (true) {}
+    
     [[NSClassFromString(@"AppDelegate") alloc] init];
     
     return UIApplicationMain(argc, argv, @"UIApplication", nil);
