5a8d7da3ac41f6b9d3a259d57d2e96a91e29e174
Samuliak <samuliak77@gmail.com>
2024-11-24 11:43:25 +0100

    add placeholder UI

--- Tequila/Application.m
+++ Tequila/Application.m
@@ -5,15 +5,114 @@
 //  Created by Samuliak on 18/11/2024.
 //
 
-#include "Tequila.h"
+#include "Common.h"
 
-#import <Foundation/Foundation.h>
-#import <UIKit/UIKit.h>
+@interface ViewController : UIViewController
+
+@end
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    self.view.backgroundColor = [UIColor yellowColor];
+}
+
+
+@end
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow* window;
+
+@end
+
+@interface SceneDelegate ()
+
+@end
+
+@implementation SceneDelegate
+
+- (void)scene:(UIScene *)scene willConnectToSession:(UISceneSession *)session options:(UISceneConnectionOptions *)connectionOptions {
+    if ([scene isKindOfClass:[UIWindowScene class]]) {
+        UIWindowScene *windowScene = (UIWindowScene *)scene;
+        self.window = [[UIWindow alloc] initWithWindowScene:windowScene];
+        
+        // Set the root view controller
+        ViewController *rootViewController = [[ViewController alloc] init];
+        self.window.rootViewController = rootViewController;
+
+        [self.window makeKeyAndVisible];
+    }
+}
+
+- (void)sceneDidDisconnect:(UIScene *)scene {
+    // Called as the scene is being released by the system.
+    // This occurs shortly after the scene enters the background, or when its session is discarded.
+    // Release any resources associated with this scene that can be re-created the next time the scene connects.
+    // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
+}
+
+- (void)sceneDidBecomeActive:(UIScene *)scene {
+    // Called when the scene has moved from an inactive state to an active state.
+    // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+}
+
+- (void)sceneWillResignActive:(UIScene *)scene {
+    // Called when the scene will move from an active state to an inactive state.
+    // This may occur due to temporary interruptions (ex. an incoming phone call).
+}
+
+- (void)sceneWillEnterForeground:(UIScene *)scene {
+    // Called as the scene transitions from the background to the foreground.
+    // Use this method to undo the changes made on entering the background.
+}
+
+- (void)sceneDidEnterBackground:(UIScene *)scene {
+    // Called as the scene transitions from the foreground to the background.
+    // Use this method to save data, release shared resources, and store enough scene-specific state information
+    // to restore the scene back to its current state.
+}
+
+@end
+
+@interface MyAppDelegate : UIResponder <UIApplicationDelegate>
+
+@end
+
+@interface MyAppDelegate ()
+
+@end
+
+@implementation MyAppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    return YES;
+}
+
+- (UISceneConfiguration *)application:(UIApplication *)application configurationForConnectingSceneSession:(UISceneSession *)connectingSceneSession options:(UISceneConnectionOptions *)options {
+    // Called when a new scene session is being created.
+    // Use this method to select a configuration to create the new scene with.
+    return [[UISceneConfiguration alloc] initWithName:@"Default Configuration" sessionRole:connectingSceneSession.role];
+}
+
+- (void)application:(UIApplication *)application didDiscardSceneSessions:(NSSet<UISceneSession *> *)sceneSessions {
+    // Called when the user discards a scene session.
+    // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+    // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+}
+
+@end
 
 int tqlApplicarionMain(int argc, const char* _Nonnull argv[_Nonnull]) {
-    while (true) {}
+    //[[NSClassFromString(@"AppDelegate") alloc] init];
     
-    [[NSClassFromString(@"AppDelegate") alloc] init];
-    
-    return UIApplicationMain(argc, argv, @"UIApplication", nil);
+    return UIApplicationMain(argc, argv, nil, NSStringFromClass([MyAppDelegate class]));
 }
---
+++ Tequila/Common.h
@@ -0,0 +1,16 @@
+//
+//  Common.h
+//  Tequila
+//
+//  Created by Samuliak on 24/11/2024.
+//
+
+#ifndef Common_h
+#define Common_h
+
+#include "Tequila.h"
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+#endif /* Common_h */
